21e429c1ed53cdfeed2d2abd193ff17c9dab3130
Manikanta <sriramoju.manii1507@gmail.com>
2024-04-28 18:39:23 +0530

    Sum() Added

---
+++ src/main/java/demo/MyCalculator.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.util.Scanner;
+
+public class MyCalculator 
+{
+	int mul(int a, int b) {
+		
+		return a*b;
+		
+	}
+
+
+
+
+	public static void main(String args[]) {
+		
+		Scanner myObj = new Scanner(System.in);
+		String input = myObj.nextLine();
+		
+		System.out.println("Hi " +input+ " below is the Answer for your querry");
+		
+		MyCalculator calc = new MyCalculator();
+		System.out.println("Sum is :"+calc.mul(20, 10));
+		
+	
+	}
+
+}
